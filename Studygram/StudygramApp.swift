5232b7e1431c13cb5e14342552f1112f2502f596
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2022-07-26 10:36:33 +0700

    Initial Commit

---
+++ Studygram/StudygramApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudygramApp.swift
+//  Studygram
+//
+//  Created by Renzo Alvaroshan on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct StudygramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
